4f502401d25610be807b2bdfe7c64fc75503e105
SharifHasan-786 <sharif016693@gmail.com>
2020-09-06 10:41:35 +0800

    null vs undefined practice

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+let pakhi;
+
+function addNum(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function addNum (num1,num2){
+    console.log(num1,num2)
+}
+
+const premika = {name:"alya batty",phone: 343566,bf:"Shaher Khan"};
+console.log(premika.address)
+
+const justFun = undefined;
+console.log(justFun)
+
+const ages = [23,45,67];
+console.log(ages[5])
+
+
+
